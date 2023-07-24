44c70ebe0c807f9eb0467a47af3a9247073738c2
Abhipsha Neog <abhipshaneog0381@gmail.com>
2023-07-24 15:08:30 +0530

    modified:   config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     // JWT secret for token generation and validation
   JWT_SECRET: 'your_jwt_secret_key',
    // Secret code for admin registration
-    secretAdminCode: 'your-secret-admin-code'
+    secretAdminCode: 'your-secret-admin-code',
   };
   
