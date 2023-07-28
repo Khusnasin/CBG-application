329d46ea08e8b2327a63bbe3f02b27bc2dcde80b
Abhipsha Neog <abhipshaneog0381@gmail.com>
2023-07-29 03:17:26 +0530

    modified:   client 	modified:   routes/statistics.js

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,6 +1,51 @@
-// routes/api/statistics.js
-
 const express = require('express');
+const router = express.Router();
+const Farmer = require('../models/farmer'); // Assuming you have a Farmer model
+
+// Helper function to fetch statistics data based on the selected option
+const getStatisticsData = async (selectedOption) => {
+  let statisticsData;
+
+  if (selectedOption === 'Napier Grass') {
+    // Fetch statistics data for Napier Grass
+    statisticsData = await Farmer.find({ /* Your filter criteria for Napier Grass */ });
+  } else if (selectedOption === 'Cows') {
+    // Fetch statistics data for Cows
+    statisticsData = await Farmer.find({ /* Your filter criteria for Cows */ });
+  } else if (selectedOption === 'both') {
+    // Fetch statistics data for both Napier Grass and Cows
+    statisticsData = await Farmer.find({ /* Your filter criteria for both */ });
+  } else {
+    // Invalid selectedOption, return null
+    statisticsData = null;
+  }
+
+  return statisticsData;
+};
+
+// GET request to fetch statistics data based on the selected option
+router.get('/:selectedOption', async (req, res) => {
+  const { selectedOption } = req.params;
+
+  try {
+    const statisticsData = await getStatisticsData(selectedOption);
+
+    if (statisticsData === null) {
+      return res.status(400).json({ error: 'Invalid selectedOption' });
+    }
+
+    // Return the statistics data as the JSON response
+    res.json(statisticsData);
+  } catch (error) {
+    console.error('Error fetching statistics data:', error);
+    res.status(500).json({ error: 'Failed to fetch statistics data' });
+  }
+});
+
+module.exports = router;
+
+
+/*const express = require('express');
 const router = express.Router();
 const Farmer = require('../models/farmer'); // Assuming you have a Farmer model
 
@@ -13,13 +58,13 @@ router.get('/:selectedOption', async (req, res) => {
 
     if (selectedOption === 'Napier Grass') {
       // Fetch statistics data for Napier Grass
-      statisticsData = await Farmer.find({ /* Your filter criteria for Napier Grass */ });
+      statisticsData = await Farmer.find({ /* Your filter criteria for Napier Grass );
     } else if (selectedOption === 'Cows') {
       // Fetch statistics data for Cows
-      statisticsData = await Farmer.find({ /* Your filter criteria for Cows */ });
+      statisticsData = await Farmer.find({ /* Your filter criteria for Cows );
     } else if (selectedOption === 'both') {
       // Fetch statistics data for both Napier Grass and Cows
-      statisticsData = await Farmer.find({ /* Your filter criteria for both */ });
+      statisticsData = await Farmer.find({ /* Your filter criteria for both );
     } else {
       // Invalid selectedOption, return an error response
       return res.status(400).json({ error: 'Invalid selectedOption' });
@@ -34,3 +79,4 @@ router.get('/:selectedOption', async (req, res) => {
 });
 
 module.exports = router;
+*/
