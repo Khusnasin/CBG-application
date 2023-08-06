5c0ae02cf9fe807cadbde7726801cc77c21689db
Subham Roy <itssubhamroy@gmail.com>
2023-08-06 23:41:31 +0530

    Updated push

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dbConfig = require('./db')
 const farmersRoute = require('./routes/farmersRoute');
 const usersRoute = require('./routes/usersRoute');
 const adminRoute = require('./routes/adminRoute');
+const path = require('path')
 //const statisticsRoute = require('./routes/statistics');
 
 app.use(express.json());
@@ -16,6 +17,15 @@ app.use('/api/admins' , adminRoute);
 //app.use(statisticsRoute);
 
 
+// static files
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
+
+
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Node server started using nodemon')); 
+app.listen(port, () => console.log('Node server started using nodemon'));
